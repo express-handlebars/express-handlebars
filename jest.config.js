9cd1a5a5b1b390b37b146281dd3c68c216a3fccb
Tony Brix <tony@brix.ninja>
2020-04-02 21:01:10 -0500

    comment jest config

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,15 @@ module.exports = {
 		"!**/node_modules/**",
 	],
 	coverageDirectory: "coverage",
-	// coverageThreshold: {
-	// 	global: {
-	// 		branches: 100,
-	// 		functions: 100,
-	// 		lines: 100,
-	// 		statements: 100,
-	// 	},
-	// },
+	/*
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
+	*/
 	testRegex: /\.test\.jsx?/.source,
 };
