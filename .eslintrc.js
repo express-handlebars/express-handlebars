a9b8a36f3f36bd9febb60c87ac22024d65b5bd5c
Tony Brix <tony@brix.ninja>
2020-04-02 19:59:26 -0500

    setup tests, github action, and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		node: true,
+		jest: true,
+	},
+	extends: [
+		'standard',
+	],
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		'no-warning-comments': 'warn',
+		'comma-dangle': ['error', 'always-multiline'],
+		indent: ['error', 'tab', { "SwitchCase": 1 }],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'no-restricted-globals': [
+            'error',
+            {
+                name: 'fit',
+                message: 'Do not commit focused tests.'
+            },
+            {
+                name: 'fdescribe',
+                message: 'Do not commit focused tests.'
+            },
+        ],
+	},
+}
