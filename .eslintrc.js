d8083b42b8c004e070d2c29e28eca3e1d10d9822
Tony Brix <tony@brix.ninja>
2020-05-08 19:42:47 -0500

    chore(lint): lint eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,22 +10,22 @@ module.exports = {
 		ecmaVersion: 2018,
 	},
 	rules: {
-		"quotes": ["error", "double"],
-		"semi": ["error", "always"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
 		"no-warning-comments": "warn",
 		"comma-dangle": ["error", "always-multiline"],
-		indent: ["error", "tab", { "SwitchCase": 1 }],
+		indent: ["error", "tab", { SwitchCase: 1 }],
 		"no-tabs": "off",
 		"no-restricted-globals": [
-            "error",
-            {
-                name: "fit",
-                message: "Do not commit focused tests."
-            },
-            {
-                name: "fdescribe",
-                message: "Do not commit focused tests."
-            },
-        ],
+			"error",
+			{
+				name: "fit",
+				message: "Do not commit focused tests.",
+			},
+			{
+				name: "fdescribe",
+				message: "Do not commit focused tests.",
+			},
+		],
 	},
-}
+};
